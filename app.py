dd3faff623e4287b8af95a8a837fa6ecc1667fe7
Manoel Victor <medeiros.manoel@escolar.ifrn.edu.br>
2026-04-22 17:30:35 -0300

    rotas primarias e arquivos html

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
+user = None
 
 @app.route('/')
 def index():
-    render_template("index.html")
+    return render_template("index.html")
+
+@app.route('/cadastro')
+def cadastrar():
+    if user:
+        return redirect(url_for('dash'))
+    return render_template('cadastro.html')
+
+@app.route('/login', methods=['post'])
+def login():
+    global user
+    if user:
+        return redirect(url_for('dash'))
+    user = request.form.get('nome')
+    senha = request.form.get('senha')
+    return redirect(url_for('dash'))
+
+@app.route('/dash')
+def dash():
+    if not user:
+        return redirect(url_for('cadastro'))
+    return render_template('dash.html', user = user)
+
+@app.route('/logout', methods=['post'])
+def logout():
+    global user
+    user = None
+    return redirect(url_for('index'))
