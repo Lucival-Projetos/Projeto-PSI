651dd2b1f5d6ee4532799ab715c60877a3d77a42
Lucival Alves <alves.lucival@escolar.ifrn.edu.br>
2026-05-29 22:58:01 -0300

    Adiciona listagem da tabela de buracos por usuario

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Trabalho todinho
+# *Sistema de Gestão de Buracos nas Ruas - RuaHole*
+
+# Descrição
+Este projeto é um site desenvolvido em Python, HTML, CSS & JavaScript.
+O objetivo é possibilitar que o usuário registre buracos que estão presentes em ruas.
+
+# Funcionalidades
+- Permite o usuário relatar e descrever problemas de buracos em ruas.
+- É possível editar e apagar um formulário já enviado.
+- O usuário pode fechar a sessão.
+- Possibilita o usuário vizualizar formulários já enviados.
+- O usuário pode descrever detalhadamente o problema.
+- Verifica se todos os campos foram preenchidos.
+
+# Como rodar
+1. Baixe os arquivos do projeto.
+2. Abra seu editor de código desejado e abra a pasta do arquivo.
+3. Crie um ambiente.
+4. Instale o Flask.
+5. Rode o projeto e abra no navegador.
+
+# Grupo
+- Alisson Bezerra (CSS / BackEnd da página)
+- Manoel Victor (JavaScript)
+- Lucival Alves (HTML e Python)
+- Gabriel de Medeiros Lima (Tudo)
--- app.py
+++ app.py
@@ -1,35 +1,190 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, flash, abort, session
+from .db import criar_conexao, inicializar_banco
 
 app = Flask(__name__)
-user = None
+app.config['SECRET_KEY'] = '00000'
 
-@app.route('/')
+inicializar_banco()
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
-
-@app.route('/cadastro')
-def cadastrar():
-    if user:
+    if 'user' in session:
         return redirect(url_for('dash'))
+    return render_template("cadastro.html")
+
+@app.route('/cadastroBuracos')
+def cadastro_buracos():
+    if 'user' not in session:
+        abort(401)
+    return render_template('cadastroBuracos.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if 'user' in session:
+        return redirect(url_for('dash'))
+    
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+
+        if not nome or not email or not senha:
+            flash("Preencha todos os campos!", "error")
+            return redirect(url_for('cadastro'))
+
+        conexao = criar_conexao()
+        resultado = conexao.execute("SELECT * FROM usuarios WHERE nome == ?", (nome,))
+        user = resultado.fetchone()
+
+        if not user:
+            conexao.execute("INSERT INTO usuarios(nome, email,senha) VALUES (?,?,?)", (nome, email, senha))
+            conexao.commit()
+            conexao.close()
+            return redirect(url_for('login'))
+        
+        else:
+            flash('usuário existente')
+            return redirect(url_for('cadastro'))
+
     return render_template('cadastro.html')
 
-@app.route('/login', methods=['post'])
+@app.route('/login', methods=['GET', 'post'])
 def login():
-    global user
-    if user:
+    if 'user' in session:
         return redirect(url_for('dash'))
-    user = request.form.get('nome')
-    senha = request.form.get('senha')
-    return redirect(url_for('dash'))
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        senha = request.form.get('senha')
+
+        if not nome or not senha:
+            flash("Preencha todos os campos!", "error")
+            return redirect(url_for('login'))
+
+        conexao = criar_conexao()
+
+        resultado = conexao.execute("SELECT * FROM usuarios WHERE nome == ?", (nome,))
+        user = resultado.fetchone()
+
+        if user and user['nome'] == nome and user['senha'] == senha:
+            session['user'] = nome
+            session['id'] = user['id']
+            return redirect(url_for('dash'))
+        else:
+            flash('usuário ou senha incorreto(s)')
+            return redirect(url_for('login'))
+    
+    return render_template('login.html')
 
 @app.route('/dash')
 def dash():
-    if not user:
-        return redirect(url_for('cadastro'))
-    return render_template('dash.html', user = user)
+    if 'user' in session:
+        return render_template('dash.html', user = session['user'])
+    abort(401)
 
-@app.route('/logout', methods=['post'])
+@app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    global user
-    user = None
+    session.pop('user', None)
+    session.pop('id', None)
     return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def error404(error):
+    return render_template('error/error404.html'), 404
+
+@app.errorhandler(401)
+def error401(error):
+    return render_template('error/error401.html'), 401
+
+# Mecher a partir daqui para adicionar as funcionalidades dos buracos, OBS a tabela de buracos já foi criada no banco de dados
+
+@app.route('/cadastrar_buraco', methods=['POST'])
+def cadastrar_buraco():
+    if 'user' not in session:
+        abort(401)
+
+    if not nova_rua or not novo_bairro:
+        flash("Preencha todos os campos!", "error")
+        return redirect(url_for('index'))
+    
+
+    nova_rua = request.form.get('rua')
+    novo_bairro = request.form.get('bairro')
+    nova_referencia = request.form.get('referencia')
+    nova_gravidade = request.form.get('gravidade')
+
+    
+    novo_registro = {
+        "rua": nova_rua,
+        "bairro": novo_bairro,
+        "referencia": nova_referencia,
+        "gravidade": nova_gravidade
+    }
+
+
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    cursor.execute("INSERT INTO buracos (nome_rua,nome_bairro,nome_gravidade,referencial,usu_id) VALUES (?,?,?,?,?)", (novo_registro['rua'],novo_registro['bairro'],novo_registro['gravidade'],novo_registro['referencia'],session['id']))
+    conexao.commit()
+    cursor.execute("SELECT * FROM buracos WHERE usu_id = ?", (session['id'],))
+    buracos = cursor.fetchall()
+    conexao.close()
+
+    
+    return redirect(url_for('exibir_lista'))
+
+@app.route('/lista', methods=['post', 'get'])
+def exibir_lista():
+    if 'user' not in session:
+        abort(401)
+
+    conexao = criar_conexao()
+    cursor = conexao.cursor()
+    cursor.execute("SELECT * FROM buracos WHERE usu_id = ?", (session['id'],))
+
+    buracos = cursor.fetchall()
+    conexao.close()
+
+    return render_template('lista.html', buracos=buracos)
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    global lista_buracos
+    
+    nova_lista_buracos = []
+    for buraco in lista_buracos:
+        if buraco['id'] != id:
+            nova_lista_buracos.append(buraco)
+    
+    lista_buracos = nova_lista_buracos
+
+    for i, buraco in enumerate(lista_buracos):
+        buraco['id'] = i + 1  
+    
+    flash("Relato removido e IDs reordenados.", "warning")
+    return redirect(url_for('exibir_lista'))
+
+@app.route('/editar/<int:id>', methods=['GET', 'POST'])
+def editar(id):
+    if 'user' not in session:
+        abort(401)
+
+    buraco_procurado = None
+
+    for buraco in lista_buracos:
+        if buraco['id'] == id:
+            buraco_procurado = buraco
+            break
+
+    if buraco_procurado is None:
+        abort(404)
+
+    if request.method == 'POST':
+        buraco_procurado['rua'] = request.form.get('rua')
+        buraco_procurado['bairro'] = request.form.get('bairro')
+        buraco_procurado['referencia'] = request.form.get('referencia')
+        buraco_procurado['gravidade'] = request.form.get('gravidade')
+        
+        flash("Informações atualizadas!", "info")
+        return redirect(url_for('exibir_lista'))
+
+    return render_template('editar.html', buraco=buraco_procurado)
